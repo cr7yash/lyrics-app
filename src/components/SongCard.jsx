a88669f647f0743188dcd081b105f8d65b433b59
yash <yashaqua@gmail.com>
2022-10-05 14:50:27 +0530

    add play pause functionality

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,7 +3,7 @@ import {useDispatch} from "react-redux";
 import PlayPause from "./PlayPause";
 import {playPause, setActiveSong} from "../redux/features/playerSlice";
 
-const SongCard = ({song, activeSong, i, isPlaying}) => {
+const SongCard = ({song, activeSong, i, isPlaying, data}) => {
   const dispatch = useDispatch();
 
   const handlePauseClick = () => {
@@ -18,11 +18,11 @@ const SongCard = ({song, activeSong, i, isPlaying}) => {
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-        // className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
-        //   activeSong?.title === song.title
-        //     ? "flex bg-black bg-opacity-70"
-        //     : "hidden"
-        // }`}
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong?.title === song.title
+              ? "flex bg-black bg-opacity-70"
+              : "hidden"
+          }`}
         >
           <PlayPause
             isPlaying={isPlaying}
