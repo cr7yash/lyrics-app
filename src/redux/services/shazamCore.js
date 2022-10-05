eb727798a415928fae98bf6a2e5d5b845dbfee61
yash <yashaqua@gmail.com>
2022-10-05 14:42:38 +0530

    Songcard component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -8,7 +8,7 @@ export const shazamCoreApi = createApi({
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        `${process.env.VITE_SHAZAM_CORE_RAPID_API_KEY}`
+        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
       );
       return headers;
     },
